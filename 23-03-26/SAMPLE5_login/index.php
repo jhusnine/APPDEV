d0bc9b9025b9ad03a85c99dcdcdbfbb299315f95
jhusnine <marianojhusnine@gmail.com>
2026-03-23 10:18:26 +0800

    Create index.php

---
+++ 23-03-26/SAMPLE5_login/index.php
@@ -0,0 +1,12 @@
+<body>
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<p id="message"></p>
+	<div>
+		Username: <input type="text" id="txtusername"/>
+	</div>
+	<div>
+		Password: <input type="text" id="txtpassword"/>
+	</div>
+	<button id="btnlogin">Login</button> 
+</body>
+<script src="login.js?v=<?= time() ?>"></script>
