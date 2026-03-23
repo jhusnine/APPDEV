7650cec6b625b8dcef6b45e087773db4d53f2f2f
jhusnine <marianojhusnine@gmail.com>
2026-03-23 10:20:49 +0800

    Create load_students.php

---
+++ 23-03-26/SAMPLE5_login/SAMPLE6_search/lod_students.php
@@ -0,0 +1,25 @@
+<?php
+$host = "10.70.0.55";
+$dbname = "students";
+$user = "boot";
+$password = "boot";
+
+try {
+	$name = $_GET["name"];
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$sql = "SELECT `basicinfo_id` AS `Id`,
+		`basicinfo_lname` AS `Lastname`,
+		`basicinfo_fname` AS `Firstname`,
+		`basicinfo_mname` AS `Middlename`
+	 FROM `basicinformation`
+	WHERE CONCAT(`basicinfo_lname`,`basicinfo_fname`,`basicinfo_mname`)
+	LIKE  CONCAT('%',?,'%');";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute([$name]);
+	$student = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($student);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
